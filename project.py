34298c3e7bb555920bb81262e8782b7a04ee081d
Ian Ballard <iancballard@gmail.com>
2016-12-19 13:38:11 -0800

    lyman project file

---
+++ project.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# Configuration file for mag built on Fri Apr 29 15:50:27 2016
+
+# Default experiment name
+default_exp = 'mag'
+
+# Data directory is where the input data lives
+data_dir = '/home/iballard/mag/data'
+
+# Analysis directory is where anything written by these scripts will live
+analysis_dir = '/home/iballard/mag/analysis'
+
+# Working directory is where data lives during workflow execution
+working_dir = '/home/iballard/mag/analysis/workingdir'
+
+# Crash directory is where debugging info will be written if things go wrong
+crash_dir = '/home/iballard/mag/crashes/'
+
+# Set this to True to remove the working directory after each excecution
+rm_working_dir = False
+
+# Should normalization be performed using ANTS?
+ants_normalization = True
+
